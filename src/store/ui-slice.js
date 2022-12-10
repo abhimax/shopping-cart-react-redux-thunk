53cd381ca248fa343c5fb1b918733d194595c087
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-10 06:15:51 +0530

    feat: add notification module

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,15 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUiState = { isCartVisible: false };
+const initialUiState = { isCartVisible: false, notification: null };
 const uiSlice = createSlice({
-    name: 'ui',
-    initialState: initialUiState,
-    reducers: {
-        toggle (state) {
-            state.isCartVisible = !state.isCartVisible;
-        }
-    }
-     
+  name: "ui",
+  initialState: initialUiState,
+  reducers: {
+    toggle(state) {
+      state.isCartVisible = !state.isCartVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 
 export default uiSlice;
