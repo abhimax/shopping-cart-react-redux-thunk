99c00362050bbcfbbc1e19e0b9040accff1b19f3
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-12-11 08:20:22 +0530

    feat: add redux thunk instead of component logic

--- src/App.js
+++ src/App.js
@@ -1,62 +1,41 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import Notification from './components/UI/Notification';
-import { uiAction } from './store/ui-slice';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
+
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
-  const showCart = useSelector( state => state.ui.isCartVisible);
-  const cart = useSelector(state => state.cart);
-  const notification = useSelector(state => state.ui.notification);
+  const showCart = useSelector((state) => state.ui.isCartVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
-  useEffect(()=>{
-    const sendCardData = async () => {
-      dispatch(uiAction.showNotification({
-        status : 'pending',
-        title: 'Sending...',
-        message: 'Sending card data!'
-      }));
-    const response = await fetch('https://movie-http-c62a0-default-rtdb.firebaseio.com/cart.json',{
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    });
-
-    if( !response.ok){
-      throw new Error('Sending card Data Failed.');
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
     }
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
-    dispatch(uiAction.showNotification({
-      status : 'success',
-      title: 'Success!',
-      message: 'Sent card data successfully!'
-    }));
-  };
-
-  if (isInitial) {
-    isInitial = false;
-    return;
-  }
-    sendCardData().catch( error => {
-      dispatch(uiAction.showNotification({
-        status : 'error',
-        title: 'Error!',
-        message: 'Sent card data failed!'
-      }))
-    });
-    
-  },[cart]);
   return (
     <>
-    { notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>}
-    <Layout>
-    { showCart && <Cart />}
-    <Products />
-  </Layout>
-  </>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiAction } from "./ui-slice";
 
 const initialCartState = {
   items: [],
@@ -38,6 +39,50 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiAction.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending card data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://movie-http-c62a0-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending card Data Failed.");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        uiAction.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent card data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiAction.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sent card data failed!",
+        })
+      );
+    }
+  };
+};
 
 export default cartSlice;
 export const cartActions = cartSlice.actions;
